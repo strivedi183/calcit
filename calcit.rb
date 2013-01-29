ab8ff8b902044c405039550f96178c45feb7eb88
Luke Walker <luke.t.walker@gmail.com>
2013-01-29 15:37:57 -0500

    my intro

---
+++ calcit.rb
@@ -0,0 +1,20 @@
+puts "What would you like to do?"
+puts "Use Basic Calculator?, Use Advanced Calculator, Quit?"
+you = gets.chomp
+
+
+
+
+
+
+if you = "Use Basic Calculator?"
+  #take you to basic Calculator
+end
+
+if you = "Use Advanced Calculator"
+  #take you to advanced calculator
+end
+
+if you = "Quit"
+  #exit the program
+end
