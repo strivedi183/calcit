8f39d85fb0a7a95643ec8cdff197aa66b3873ddc
Sandip Trivedi <trivedi.sandip@gmail.com>
2013-01-29 18:11:21 -0500

    Small changes to mortgage but everything works

--- main.rb
+++ main.rb
@@ -61,13 +61,14 @@ def mort_calc
   puts "Welcome to the mortage calculator."
   print "Please enter your loan principal. "
   loan_principal = gets.chomp.to_f
-  print "Please enter your interest rate. "
+  print "Please enter your interest rate (monthly). "
   interest_rate = gets.chomp.to_f
-  print "Please enter the number of payments you have made. "
+  print "Please enter the number of payments. "
   num_payments = gets.chomp.to_f
   calc = (1 + interest_rate)**num_payments
   monthly_payment = loan_principal * ((interest_rate * calc) / (calc - 1))
-  puts "Your monthly mortage payment is $#{monthly_payment.round(2)}"
+  puts "Your monthly mortage payment is $#{monthly_payment.round(2)} (Press Enter key to continue)"
+  gets
 end
 
 while you != "q"
