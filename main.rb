ac7fc0dd1ea4abffa90ecfc5630fb1b2d31b7492
Sandip Trivedi <trivedi.sandip@gmail.com>
2013-01-29 15:41:40 -0500

    Made it a function for basic calc

--- main.rb
+++ main.rb
@@ -1,21 +1,23 @@
-# User chooses math action and enters their numbers
-print "Would you like to add, subtract, multiply, or divide? "
-math_action = gets.chomp
-print "Please enter your first number: "
-first_number = gets.chomp.to_i
-print "Please enter your second number: "
-second_number = gets.chomp.to_i
+def basic_calc
+  # User chooses math action and enters their numbers
+  print "Would you like to add, subtract, multiply, or divide? "
+  math_action = gets.chomp
+  print "Please enter your first number: "
+  first_number = gets.chomp.to_i
+  print "Please enter your second number: "
+  second_number = gets.chomp.to_i
 
-#This is where the numbers have math actions done against them
-case math_action
-  when "add"
-    puts "The answer is #{first_number + second_number}"
-  when "subtract"
-    puts "The answer is #{first_number - second_number}"
-  when "multiply"
-    puts "The answer is #{first_number * second_number}"
-  when "divide"
-    puts "The answer is #{first_number / second_number}"
-  else
-    print "error"
+  #This is where the numbers have math actions done against them
+  case math_action
+    when "add"
+      puts "The answer is #{first_number + second_number}"
+    when "subtract"
+      puts "The answer is #{first_number - second_number}"
+    when "multiply"
+      puts "The answer is #{first_number * second_number}"
+    when "divide"
+      puts "The answer is #{first_number / second_number}"
+    else
+      print "error"
+  end
 end
